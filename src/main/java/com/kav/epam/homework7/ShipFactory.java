2cd9fcbc1abe6b12609bae54e1553062394e45f4
geserok <geserok@gmail.com>
2018-11-12 16:46:30 +0300

    Working commit 12.11.18 added homework7 package with classes which appeared Ships.

--- src/main/java/com/kav/epam/homework7/ShipFactory.java
+++ src/main/java/com/kav/epam/homework7/ShipFactory.java
@@ -5,24 +5,32 @@ public class ShipFactory {
 
     public Ship createShip(int size, int xCoordinateShipHead, int yCoordinateShipHead,
                            int xCoordinateShipStern, int yCoordinateShipStern) {
-        switch (size) {
-            case 1:
-                ship = new SingleDeckShip(xCoordinateShipHead, yCoordinateShipHead,
-                        xCoordinateShipStern, yCoordinateShipStern);
-                break;
-            case 2:
-                ship = new DoubleDeckShip(xCoordinateShipHead, yCoordinateShipHead,
-                        xCoordinateShipStern, yCoordinateShipStern);
-                break;
-            case 3:
-                ship = new ThreeDeckShip(xCoordinateShipHead, yCoordinateShipHead,
-                        xCoordinateShipStern, yCoordinateShipStern);
-                break;
-            case 4:
-                ship = new FourDeckShip(xCoordinateShipHead, yCoordinateShipHead,
-                        xCoordinateShipStern, yCoordinateShipStern);
-                break;
+        coordinateCheck(xCoordinateShipHead);
+        coordinateCheck(yCoordinateShipHead);
+        coordinateCheck(xCoordinateShipStern);
+        coordinateCheck(yCoordinateShipStern);
+
+        if (size == 1) {
+            ship = new SingleDeckShip(xCoordinateShipHead, yCoordinateShipHead,
+                    xCoordinateShipStern, yCoordinateShipStern);
+        } else if (size == 2) {
+            ship = new DoubleDeckShip(xCoordinateShipHead, yCoordinateShipHead,
+                    xCoordinateShipStern, yCoordinateShipStern);
+        } else if (size == 3) {
+            ship = new ThreeDeckShip(xCoordinateShipHead, yCoordinateShipHead,
+                    xCoordinateShipStern, yCoordinateShipStern);
+        } else if (size == 4) {
+            ship = new FourDeckShip(xCoordinateShipHead, yCoordinateShipHead,
+                    xCoordinateShipStern, yCoordinateShipStern);
+        } else {
+            throw new IllegalArgumentException("Wrong size of the ship!");
         }
         return ship;
+    }
+
+    public void coordinateCheck(int coordinate) {
+        if (coordinate < 0 || coordinate > 9) {
+            throw new IllegalArgumentException("Wrong coordinate!");
+        }
     }
 }
